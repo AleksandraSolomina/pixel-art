8ba108706709464f97ccb2e56e68772a3baa909d
AleksandraSolomina <71250590+AleksandraSolomina@users.noreply.github.com>
2020-09-24 20:44:56 +0500

    Add files via upload

--- canvas.js
+++ canvas.js
@@ -1,21 +1,92 @@
 const canvasContainer = document.getElementById("canvas-container");
-const particles = document.getElementsByClassName("particle");
-const paletteContainer = document.getElementById("palette-container");
-const theColor;
+const particles = document.querySelectorAll(".particle");
+const paletteContainer = document.querySelector(".palette-container");
+const deleteButton = document.querySelector("input");
 const colors = document.getElementsByClassName("palette-item");
+const current = document.querySelector(".current-color");
+const colorPicker = document.getElementById("color-picker");
+const loadButton = document.getElementById("load-button");
+const saveButton = document.getElementById("save-button");
 
+let theColor = "";
+
+//picking the color
+paletteContainer.addEventListener("click", function(e) {
+    switch(e.target.id) {
+        case "red":
+        theColor = "red";
+        current.style.backgroundColor = theColor;
+        break;
+        case "yellow":
+        theColor = "yellow";
+        current.style.backgroundColor = theColor;
+        break;
+        case "black":
+        theColor = "black";
+        current.style.backgroundColor = theColor;
+        break;
+    };
+});
+
+//changing the color of a pixel on click
 canvasContainer.addEventListener("click", function(e) {
     if (e.target.classList.contains("particle")){
     e.target.style.backgroundColor = theColor;
 }});
-for (let color of colors){
-color.addEventListener("click", function() {
-    theColor = color.style.background;
+
+
+//bonus 1: changing the color of pixels 
+//by pressing and dragging(like a paintbrush)
+
+canvasContainer.addEventListener("mousedown", activateBrush);
+canvasContainer.addEventListener("mouseup", deactivateBrush); 
+
+function activateBrush(){
+    for (particle of particles){
+        particle.addEventListener("mouseenter", changeColor);
+    }
+}
+function changeColor(){
+    this.style.backgroundColor = theColor;
+}
+function deactivateBrush(){
+    for (particle of particles){
+        particle.removeEventListener("mouseenter", changeColor);
+    }
+}
+
+//button for clearing the canvas
+deleteButton.addEventListener("click", function(){
+    for (particle of particles){
+        particle.style.backgroundColor = "";
+    }
 })
-};
+
+// bonus 2: adding color picker
+colorPicker.addEventListener("input", function(e){
+    current.style.backgroundColor = e.target.value;
+});
+colorPicker.addEventListener("change", function(e){
+    theColor = e.target.value;
+})
 
 
+// bonus 3: save and load the drawing to/from the Local storage
+saveButton.addEventListener('click', function() {
+let customPicture = [];
+for (particle of particles){
+customPicture.push(particle.style.backgroundColor);
+}
 
+localStorage.clear();
+localStorage.setItem('customPicture', JSON.stringify(customPicture));
 
-//create some 'theColor' to insert ^^ and further usage
-//
+});
+
+loadButton.addEventListener('click', function() {
+    let picture = JSON.parse(localStorage.getItem('customPicture'));
+
+	for (let i = 0; i <= picture.length - 1; i++) {
+		particles[i].style.backgroundColor = picture[i];
+	}
+});
